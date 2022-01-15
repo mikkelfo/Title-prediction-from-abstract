9027b8694922de2da48f926a3f937c6944f6f61a
mikkelfo <mikk528a@gmail.com>
2022-01-15 15:04:21 +0100

    Main changes

--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 from src.data.dataModule import ArvixDataModule
 from src.models.model import PredNet
-from src.models.utils import store_model
+from src.models.utils import save_model
 
 
 
 if __name__ == '__main__':
-    # wandb_logger = WandbLogger()
-    # dm = ArvixDataModule()
+    wandb_logger = WandbLogger()
+    dm = ArvixDataModule()
     model = PredNet()
-    # trainer = Trainer(max_epochs=2, logger=wandb_logger)
-    # trainer.fit(model, dm)
+    trainer = Trainer(max_epochs=2, logger=wandb_logger)
+    trainer.fit(model, dm)
 
-    store_model(model)
+    save_model(model)
