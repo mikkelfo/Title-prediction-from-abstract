fec5a4f67321532b003dea261073ade88eb5a7b5
Ole-Batting <batting.ole@gmail.com>
2022-01-20 09:34:14 +0100

    less data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import EarlyStopping
 
 from src.data.dataModule import ArvixDataModule
 from src.models.model import PredNet
@@ -13,6 +14,10 @@ if __name__ == "__main__":
     wandb_logger = WandbLogger(project = "arxiv", entity = "title-generation")
     dm = ArvixDataModule()
     model = PredNet()
-    trainer = Trainer(max_epochs=10, logger=wandb_logger, accelerator="gpu", gpus=1)
+    early_stopping_callback = EarlyStopping(
+        monitor="validation_loss", patience=3, verbose=True, mode="min"
+    )
+    trainer = Trainer(max_epochs=10, logger=wandb_logger, accelerator="gpu", gpus=1,
+                      callbacks=[early_stopping_callback])
     trainer.fit(model, dm)
     save_model(model)
